eb1640219654504f081ffb6f2a4cdcdf4f4aa761
sweta-singh28 <swetasinghgs28@gmail.com>
2025-03-24 23:29:30 +0530

    changes

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function addItem() {
   const price = parseFloat(document.getElementById("price").value);
 
   const tableBody = document.getElementById("inventoryTableBody");
-  const totalAmountElement = document.getElementById("totalAmount");
 
   const row = tableBody.insertRow();
   const cell1 = row.insertCell(0);
@@ -24,29 +23,6 @@ function removeItem(button) {
   const row = button.parentNode.parentNode;
   row.parentNode.removeChild(row);
   updateTotalAmount();
-}
-
-function sellItem() {
-  const sellQuantity = parseInt(document.getElementById("sellQuantity").value);
-  const tableBody = document.getElementById("inventoryTableBody");
-  const totalAmountElement = document.getElementById("totalAmount");
-
-  const selectedRow = tableBody.querySelector("tr.selected");
-  if (selectedRow) {
-    const quantityCell = selectedRow.cells[1];
-    let quantity = parseInt(quantityCell.textContent);
-
-    
-    if (quantity >= sellQuantity) {
-      quantity -= sellQuantity;
-      quantityCell.textContent = quantity;
-      updateTotalAmount();
-    } else {
-      alert("Not enough quantity in inventory to sell.");
-    }
-  } else {
-    alert("Please select an item from the inventory to sell.");
-  }
 }
 
 function updateTotalAmount() {
